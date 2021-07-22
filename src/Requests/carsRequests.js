c84ec822c8c190b808dfd94da631024561bfe3ce
Marcin Topolski <topolskimarcin@gmail.com>
2021-07-22 12:10:13 +0200

    finish

--- src/Requests/carsRequests.js
+++ src/Requests/carsRequests.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 export const getAllCars = () => {
-    return axios.get("http://cars-app2.herokuapp.com:8080/cars");
+    return axios.get("https://cars-app2.herokuapp.com/cars");
 }
 export const getColorCars = color => {
-    return axios.get(`http://cars-app2.herokuapp.com:8080/cars/color/${color}`);
+    return axios.get(`https://cars-app2.herokuapp.com/cars/color/${color}`);
 }
